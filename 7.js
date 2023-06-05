8f8b6bd400055ff7ce20c6e803d20d7f5393d077
Vaishnavi <122547011+Vaishnavi-kota@users.noreply.github.com>
2023-06-05 18:32:43 +0530

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,12 @@
+function vowels(){
+    let x="Physics";
+   console.log(x);
+   console.log(typeof(x)); 
+ if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'){
+    var count=0;
+    count=count+x;
+   console.log(count); 
+ } 
+ return count; 
+}
+vowels();
